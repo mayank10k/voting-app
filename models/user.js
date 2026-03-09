edd4a7db7c0673d990b3a32986665c1bf27767de
Mayank Mehta <mayankmehta433@gmail.com>
2026-03-09 23:11:35 +0530

    fixed password hashing

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-// const bcrypt=require('bcrypt');
+const bcrypt=require('bcrypt');
 
 const userSchema=new mongoose.Schema({
     name:{
@@ -41,6 +41,38 @@ const userSchema=new mongoose.Schema({
     }
 
 });
+
+userSchema.pre('save',async function(){
+    const user=this;
+
+    //Hash the password only if it has been modified (or is new)
+    if(!user.isModified('password')) return ;
+    try{
+        // hash password generation
+        const salt=await bcrypt.genSalt(10);
+
+        //hash password
+        const hashedPassword=await bcrypt.hash(user.password,salt);
+
+        //override the plain password with the hashed one
+        user.password=hashedPassword
+         
+        // next();
+    }catch(err){
+        return err;
+    }
+})
+
+userSchema.methods.comparePassword=async function(candidatePassword){
+    try{
+        //use bcrypt to compare the provided password with the hashed password
+        const isMatch=await bcrypt.compare(candidatePassword,this.password);
+        return isMatch;
+
+    }catch(err){
+        throw err;
+    }
+}
 
 
 const User=mongoose.model('User',userSchema);
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express=require('express')    
 const app = express()
+const db=require('./db')
   
-// require('dotenv').config();
+require('dotenv').config();
 app.use(express.json());   
 const PORT=process.env.PORT || 3000;
+
+const userRoutes=require('./routes/userRoutes');
+
+app.use('/user',userRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:3000')
